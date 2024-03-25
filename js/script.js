bba413da88d1a37abe166d0806f04ac6dda70f98
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-03-25 22:50:59 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c)2024 George Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George Khalil
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function salaryProgram () {
+  // input
+  const hours = parseInt(document.getElementById('Your-Hours').value)
+  const rate = parseInt(document.getElementById('rate-Per-Hour').value)
+  // process
+  const takeHomeSalary = (hours * rate) * (1.00 - 0.18) 
+  const taxes = (hours * rate) * 0.18 
+
+  // output
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+document.getElementById('taxes').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+}
