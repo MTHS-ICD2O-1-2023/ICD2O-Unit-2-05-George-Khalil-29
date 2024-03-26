d509f59d76ebdbaa8ba2df099bdf512ee1b250b1
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-03-26 18:43:58 +0000

    save

--- js/script.js
+++ js/script.js
@@ -9,15 +9,19 @@
 /**
  * This function calculates area of a triangle.
  */
-function salaryProgram () {
+function salaryProgram() {
   // input
-  const hours = parseInt(document.getElementById('Your-Hours').value)
-  const rate = parseInt(document.getElementById('rate-Per-Hour').value)
+  const hoursYouwork= parseInt(document.getElementById('Hours-You-work').value)
+  const rateYouget = parseInt(document.getElementById('Rate-You-Get').value)
+  xx = parseFloat(document.getElementById('pay').value)
+
   // process
-  const takeHomeSalary = (hours * rate) * (1.00 - 0.18) 
-  const taxes = (hours * rate) * 0.18 
+  const pay = (hoursYouwork * rateYouget) * (1.00 - 0.18) ;
+  const taxes = (hoursYouwork * rateYouget) *  0.18 ;
+  const TAX_RATE = 0.18
 
   // output
-  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(1)}`
-document.getElementById('taxes').innerHTML = 'Your pay will be: $ + taxes.toFixed(2)'
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${pay.toFixed(2)}` ; 
+  document.getElementById('taxes').innerHTML = 'Your pay will be: $' + taxes.toFixed(2) ; 
+  document.getElementById('TAX_RATE').innerHTML = 'TAX_RATE is: $' + TAX_RATE.toFixed(2) ; 
 }
